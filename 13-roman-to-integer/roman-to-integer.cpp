97b3b284570e6b8ff795dcab5cb2ad1d2cb1f457
shravanisw <155523596+shravaniswagh@users.noreply.github.com>
2024-10-29 20:36:33 +0530

    Time: 8 ms (51.07%) | Memory: 14.3 MB (21.55%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int romanToInt(string s) {   
-        map <char, int> mp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-         
+    int romanToInt(string s) {
+        map<char,int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        
         int ans = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (mp[s[i]] < mp[s[i + 1]]) {
-                ans = ans -  mp[s[i]];
-            } 
-            else {
+
+        for(int i=0;i<s.length();i++){
+            if(mp[s[i]]<mp[s[i+1]]){
+                ans = ans - mp[s[i]];
+            }
+            else{
                 ans = ans + mp[s[i]];
             }
         }
